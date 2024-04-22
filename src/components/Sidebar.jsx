8051c685a5685c639a0903550edf82d34f50cbd2
Parham Abolghasemi <parhamab17@gmail.com>
2024-04-22 14:42:09 +0330

    tailwind installed, sidebar ui, react-icons installed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,34 @@
 import { NavLink } from "react-router-dom";
 import { menuItems } from "../constants/menuItems";
+import { FaBars } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const Sidebar = ({ children }) => {
+  
   return (
-    <div>
+    <div className="flex items-center">
       <div>
         <div>
           <h1>Logo</h1>
-          <div>bars</div>
+          <div>
+            <FaBars />
+            <IoMdClose />
+          </div>
         </div>
 
-        {menuItems.map((item) => (
-          <NavLink key={item?.path} to={item?.path}>
-            {item?.name}
-          </NavLink>
-        ))}
+        <ul className="w-[250px]">
+          {menuItems.map((item) => (
+            <li
+              key={item?.path}
+              className=" bg-blue-100 hover:bg-blue-950 hover:text-white transition-all flex items-center pl-2"
+            >
+              {<item.icon className="text-xl" />}
+              <NavLink to={item?.path} className={"flex flex-col p-3"}>
+                {item?.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
       <main>{children}</main>
     </div>
--- src/constants/menuItems.js
+++ src/constants/menuItems.js
@@ -1,6 +1,10 @@
+// icons
+import { FaHome, FaInfoCircle, FaShoppingBasket } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+
 export const menuItems = [
-  { path: "/", name: "home" },
-  { path: "/products", name: "products" },
-  { path: "/settings", name: "settings" },
-  { path: "/aboutUs", name: "aboutUs" },
+  { path: "/", name: "home", icon: FaHome },
+  { path: "/products", name: "products", icon: FaShoppingBasket },
+  { path: "/settings", name: "settings", icon: IoMdSettings },
+  { path: "/aboutUs", name: "aboutUs", icon: FaInfoCircle },
 ];
